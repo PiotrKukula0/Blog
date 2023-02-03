91062d0f1220e9dc2ce3b224f47fe3e26d809bf3
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-02-03 23:00:43 +0100

    styling and header

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Logo from "../img/logo.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='container'>
+        <div className='logo'>
+          <img src={Logo} alt="" />
+        </div>
+        <div className='links'>
+        <Link className='link' to="/?cat=art">
+            <h6>ART</h6>
+            </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 

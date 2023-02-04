53cbe40b57c77db78fb5b374ba99e0d1f1ac5216
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-02-04 18:36:05 +0100

    Single Post Page Design

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
+import Menu from '../components/Menu';
 
 const Single = () => {
+
+  
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className='content'>
+        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <div className='user'>
+          <img src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <div className='info'>
+            <span>Piotr</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className='edit'>
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+        <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+      </div>
+      <Menu/>
+    </div>
   )
 }
 

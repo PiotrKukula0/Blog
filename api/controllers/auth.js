c34b436d9ba0ae3370bd19c58ee3b73129a1536a
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-02-05 14:12:28 +0100

    completion of login verification

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,27 +1,27 @@
-import {db} from "../db.js"
+import { db } from "../db.js"
 import bcrypt from "bcryptjs"
-export const register = (req,res)=>{
+export const register = (req, res) => {
 
     //check existing user
 
-const q = "SELECT * FROM users WHERE email = ? Or username = ?"
+    const q = "SELECT * FROM users WHERE email = ? Or username = ?"
 
-    db.query(q,[req.body.email, req.body.username], (err,data)=>{
-        if(err) return res.json(err)
-        if(data.length) return res.status(409).json("User already exist")
+    db.query(q, [req.body.email, req.body.username], (err, data) => {
+        if (err) return res.json(err)
+        if (data.length) return res.status(409).json("User already exist")
         //hash
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
-        
+
         const q = "INSERT INTO users(`username`, `email`,`password`) VALUES (?) "
-        const values =[
+        const values = [
             req.body.username,
             req.body.email,
             hash,
 
         ]
 
-        db.query(q,[values],(err,data)=>{
+        db.query(q, [values], (err, data) => {
             if (err) return res.json(err)
             return res.status(200).json("User has been created")
         })
@@ -30,7 +30,19 @@ const q = "SELECT * FROM users WHERE email = ? Or username = ?"
 }
 
 export const login = (req, res) => {
+    //check user
 
+    const q = "SELECT * FROM users WHERE username = ?"
+
+    db.query(q, [res, body, username], (err, data => {
+        if (err) return res.json(err)
+        if (data.length === 0) return res.status(404).json("User not found")
+
+        //check password
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+
+        if(!isPasswordCorrect) return res.status(400).json("Wrong username or password")
+    })
 }
 
 export const logout = (req, res) => {
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,26 +1,29 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios"
 
 const Register = () => {
   const [inputs, setInputs] = useState({
-    username:"",
+    username: "",
     email: "",
     password: "",
   })
 
-  const handleChange = e =>{
-    setInputs(prev=>({...prev,[e.target.name]: e.target.value}))
+  const [err, setError] = useState(null)
+
+  const navigate = useNavigate()
+
+  const handleChange = e => {
+    setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }))
   }
 
-  const handleSubmit = async e =>{
+  const handleSubmit = async e => {
     e.preventDefault()
-    try{
-
-    const res = await axios.post("/auth/register",inputs )
-    console.log(res)
-    }catch(err){
-      console.log(err)
+    try {
+      await axios.post("/auth/register", inputs)
+      navigate("/login")
+    } catch (err) {
+      setError(err.response.data)
     }
   }
   return (
@@ -31,7 +34,7 @@ const Register = () => {
         <input required type="email" placeholder='email' name='email' onChange={handleChange} />
         <input required type="password" placeholder='password' name='password' onChange={handleChange} />
         <button onClick={handleSubmit}>Register</button>
-        <p>This is an error!</p>
+        {err && <p>{err}</p>}
         <span>Do you have an account?<Link to="/login">Login</Link></span>
       </form>
     </div>

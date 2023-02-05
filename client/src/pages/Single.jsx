5925adc747459788a7da406e9905fa6b76f26cac
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-02-05 19:34:55 +0100

    authentication of posts

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,33 +1,64 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react'
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import Edit from '../img/edit.png'
 import Delete from '../img/delete.png'
 import Menu from '../components/Menu';
+import axios from "axios"
+import moment from "moment"
+import { AuthContext } from '../context/authContext';
 
 const Single = () => {
+  const [post, setPost] = useState({})
 
-  
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const postId = location.pathname.split("/")[2]
+
+  const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`)
+        setPost(res.data)
+      } catch (error) {
+        // console.log(err)
+      }
+    }
+    fetchData()
+  }, [postId])
+
+  const handleDelete = async ()=>{
+    try {
+     await axios.delete(`/posts/${postId}`)
+      navigate("/")
+    } catch (error) {
+      // console.log(err)
+    }
+  }
+
   return (
     <div className='single'>
       <div className='content'>
-        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <img src={post?.img} alt="" />
         <div className='user'>
-          <img src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          { post.userImg && <img src={post.userImg} alt="" />}
           <div className='info'>
-            <span>Piotr</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className='edit'>
+          {currentUser.username === post.username && <div className='edit'>
             <Link to={`/write?edit=2`}>
               <img src={Edit} alt="" />
             </Link>
-            <img src={Delete} alt="" />
-          </div>
+            <img onClick={handleDelete} src={Delete} alt="" />
+          </div>}
         </div>
-        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-        <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+        <h1>{post.title}</h1>
+        {post.description}
       </div>
-      <Menu/>
+      <Menu />
     </div>
   )
 }
